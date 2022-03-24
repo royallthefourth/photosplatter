af7bd0a0d413aa29d19af3277c7c0fdc4ae4a9b6
Royall Spence <royall@royall.us>
2022-03-23 21:49:21 -0400

    Add Svelte frontend
    Resolves #6

--- http/http.go
+++ http/http.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 	"photosplatter/assets"
 	"photosplatter/gallery"
@@ -11,7 +12,28 @@ type photoSet struct {
 }
 
 func Index(w http.ResponseWriter, _ *http.Request) {
-	err := assets.Index.Execute(w, photoSet{Photos: gallery.GetGallery().Photos})
+	err := assets.Index.Execute(w, []byte{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AllPhotos(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("content-type", "application/json")
+	gal := gallery.GetGallery()
+	_, err := w.Write([]byte{'['})
+	if err != nil {
+		panic(err)
+	}
+	n := len(gal.Photos)
+	for i := 0; i < n; i++ {
+		_, _ = w.Write([]byte(fmt.Sprintf(`"%s"`, gal.Photos[i].Name)))
+		if i == n-1 {
+			break
+		}
+		_, _ = w.Write([]byte{','})
+	}
+	_, err = w.Write([]byte{']'})
 	if err != nil {
 		panic(err)
 	}
---
+++ js/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { each } from "svelte/internal";
+ let photos = [""];
+  fetch("/api/photos").then((resp) => {
+    return resp.json() as Promise<string[]>
+  }).then((p) => {
+    photos = p
+  })
+</script>
+
+<main>
+{#each photos as p}
+<img src="/images/{p}" alt="Photo {p}">
+{/each}
+</main>
+
+<style>
+	img {
+      width: 30%;
+      padding: 1%;
+  }
+</style>
